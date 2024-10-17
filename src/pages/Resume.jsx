046c0b6711ef420de282ad484b29a4d9ce10f208
nadeem nagarji <nadeemnagarji@gmail.com>
2024-10-17 14:41:40 +0530

    resume full view

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import resume from "../Resume.pdf";
+import resume from "../NadeemResume.pdf";
 
 const ResumePDF = () => {
   return (
-    <div>
-      <object data={resume} type="application/pdf" width="100%" height="100%">
+    <div style={{ height: "100vh", width: "100%" }}>
+      <object data={resume} type="application/pdf" width="100%" height="200%">
         <p>
           Sorry, your browser doesn't support embedded PDFs. You can download
           the PDF <a href={resume}>here</a>.
